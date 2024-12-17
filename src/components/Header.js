4c5eef4d759c8ba85d8c2eff333ae3b0146bcc6a
chennakesavapagadala <153279690+chennakesavapagadala@users.noreply.github.com>
2024-12-17 15:40:47 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header id='header'  className="header row d-flex justify-content-center p-2">
+    <header id='header'  className="header row d-flex justify-content-center p-2 text-wrap">
       <div className='col-md-1'><img className='Mylogo' src='logos/cklogo6.png' alt='Logo' height='30' width='30'></img></div>
       <nav className='col-md-11'>
         <a href="#about" >About</a>
